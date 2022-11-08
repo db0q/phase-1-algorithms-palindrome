bffd854b9e8a82626b72aea969adb7bf4a728c93
theMstafa <mm22180481@gmail.com>
2022-11-08 04:54:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-function isPalindrome(word) {
+function reverse(word)  {
   // Write your algorithm here
+  return word.split("").reverse().join("");
+  // const regex = /[\W_]/g;
+  // const lowRegWord = word.toLowerCase().replace(re, '');
+  // const reverseWord = lowRegWord.split('').reverse().join('');
+  // var len = word.length;
+  // for (var i = 0; i < len/2; i++) {
+  //   if (word[i] !== word[len - 1 - i]) { 
+  //       return false; 
+  //   }
+  //   return true;
+  // }
 }
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
 
+  return word === reversedWord;
+}
 /* 
   Add your pseudocode here
+  create a function that going to take the word and parse each 
+  letter in it ,then it will be put it in condetion 
+  the condition will return true if it palindrome and  if not it 
+  will return false
 */
 
 /*
